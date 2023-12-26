0eff15213b9034a34a3ca3558febc99f1b7ee358
unknown <renancavalcantercb@protonmail.com>
2023-12-26 05:48:35 -0300

    added a configuration file

---
+++ config.py
@@ -0,0 +1,16 @@
+import pygame
+
+game_font = "freesansbold.ttf"
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+ORANGE = (255, 165, 0)
+PURPLE = (128, 0, 128)
+
+
+money = 0
+game_screen_width = 360
+game_screen_height = 640
+screen = pygame.display.set_mode((game_screen_width, game_screen_height))
