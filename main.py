dfbd6fd799ba2d049c1d4f06ce303fcb62dbf082
unknown <renancavalcantercb@protonmail.com>
2023-12-26 05:49:18 -0300

    added menu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from menu import draw_menu
 
 pygame.init()
 
@@ -151,46 +152,68 @@ def draw_shop():
 
 
 running = True
+menu_active = True
+start_button, load_button = None, None
+
+
 while running:
-    timer.tick(frame_rate)
-    screen.fill(BLACK)
+    if menu_active:
+        menu_buttons = draw_menu()
+        pygame.display.flip()
 
-    for button in buttons:
-        button.draw(screen)
-        button.draw_level(screen, font)
-        button.update()
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                for i, menu_button in enumerate(menu_buttons):
+                    if menu_button.collidepoint(mouse_pos):
+                        if i == 0:
+                            menu_active = False
+                        if i == 1:
+                            pass
+                        if i == 2:
+                            running = False
 
-    for button_color, autoclicker in autoclickers.items():
-        autoclicker.update(
-            buttons[
-                buttons.index(
-                    next(
-                        filter(lambda b: b.button_color == button_color, buttons), None
+    else:
+        timer.tick(frame_rate)
+        screen.fill(BLACK)
+
+        for button in buttons:
+            button.draw(screen)
+            button.draw_level(screen, font)
+            button.update()
+
+        for button_color, autoclicker in autoclickers.items():
+            autoclicker.update(
+                buttons[
+                    buttons.index(
+                        next(
+                            filter(lambda b: b.button_color == button_color, buttons),
+                            None,
+                        )
                     )
-                )
-            ]
-        )
+                ]
+            )
 
-    draw_money()
-    draw_shop()
+        draw_money()
+        draw_shop()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
 
-            for button in buttons:
-                if button.clicked(mouse_pos):
-                    button.start_loading()
+                for button in buttons:
+                    if button.clicked(mouse_pos):
+                        button.start_loading()
 
-            y = screen_height - 300
-            for color, autoclicker in autoclickers.items():
-                shop_item_rect = pygame.Rect(10, y, 200, 30)
-                if shop_item_rect.collidepoint(mouse_pos):
-                    autoclicker.buy()
-                y += 40
+                y = screen_height - 300
+                for color, autoclicker in autoclickers.items():
+                    shop_item_rect = pygame.Rect(10, y, 200, 30)
+                    if shop_item_rect.collidepoint(mouse_pos):
+                        autoclicker.buy()
+                    y += 40
 
-    pygame.display.flip()
+        pygame.display.flip()
 
 pygame.quit()
